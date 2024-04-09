ac177f287fcb444129d24a2731b7ed86ed82b063
Kenneth Brook <kenneth.brook@hotmail.com>
2024-04-09 08:56:22 -0400

    tweak

--- index.js
+++ index.js
@@ -363,6 +363,21 @@ app.get('/fullPull/:clientKey', async (req, res) => {
   let whereConditions = [];
   let queryParams = [];
 
+  if (startDate && endDate) {
+    if (startDate === endDate) {
+      // Adjust endDate to include the entire day
+      const adjustedEndDate = new Date(endDate);
+      adjustedEndDate.setHours(23, 59, 59, 999); // Set to the last moment of the day
+      queryParams.push(startDate, adjustedEndDate.toISOString());
+    } else {
+      queryParams.push(startDate, endDate);
+    }
+    whereConditions.push(`creation BETWEEN $${queryParams.length - 1} AND $${queryParams.length}`);
+  } else if (month) {
+    const yearMonth = new Date(month).getFullYear();
+    queryBase += `_${yearMonth}`; // Adjust if your table naming scheme includes months
+  }
+
   // Start adding text input filters here
   if (agencyType) {
     queryParams.push(`%${agencyType}%`);
